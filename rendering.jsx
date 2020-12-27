b7bc778511e6539dc689e7b449198d9b010afb6d
nimurarikuto <nimuyohu0105@gmail.com>
2020-12-27 19:19:56 +0900

    initial commit

---
+++ rendering.jsx
@@ -0,0 +1,62 @@
+// 選択されたファイルのプレビューを表示する
+function imageFileRendering(file) {
+    // 前回表示画像を取り除く
+    $('imageContainer').remove();
+    // 指定されたファイルが画像じゃない、またはファイルの指定がない場合
+    if (file === undefined || !file.type.match('image.*')) {
+        // 送信ボタンを非活性にする
+        $('#sendAction').prop('disabled', true);
+        return false;
+    }
+    // プレビュー画像のレンダリング
+    ReactDOM.render(
+        <img id="previewImage" src={URL.createObjectURL(file)} />,
+        document.getElementById('imageContainer')
+    );
+
+    // 送信ボタンを活性にする
+    $('#sendAction').prop('disabled', false);
+
+    return true;
+};
+
+// APIのResponseのスコアをレンダリングする
+function scoreRendering(score) {
+    // TODO: スコアをレンダリングする。
+    // 前回表示点数を取り除く
+    $('result').remove();
+    $("#overlay").fadeOut(300);
+
+    // 点数のレンダリング
+    ReactDOM.render(
+        <div>
+            {(score + 1) * 10}点
+        </div>,
+        document.getElementById('result')
+    );
+};
+
+function evalRendering(difference) {
+
+    $('eval').remove();
+    var evl;
+    if (difference === 0) {
+        evl = "当たり！！";
+    } else if (difference === 1) {
+        evl = "おしい！";
+    } else {
+        evl = "残念はずれ…";
+    }
+    ReactDOM.render(
+        <div>{evl}</div>,
+        document.getElementById('eval')
+
+    );
+    $('#moodal').click();
+
+};
+
+// 他ファイルからも試用できるようwindowに登録
+window.imageFileRendering = imageFileRendering;
+window.scoreRendering = scoreRendering;
+window.evalRendering = evalRendering;
---
+++ send.jsx
@@ -0,0 +1,42 @@
+// API呼び出し時にひつようなパラメータを受ける。
+function send(file, predict) {
+
+    // APIキーとURL
+    var API_KEY = 'DZZ2gMQrsiVreHBQqPgTQXczoJieyebo';
+    var END_POINT = 'https://api.a3rt.recruit-tech.co.jp/image_influence/v1/meat_score';
+
+    // TODO: API呼び出し
+    var formdata = new FormData();
+    formdata.append('apikey', API_KEY);
+    formdata.append('imagefile', file);
+    formdata.append('predict', predict);
+    $("#overlay").fadeIn(300);
+    $.ajax({
+    type: 'POST',
+    url: END_POINT,
+    processData: false,
+    contentType: false,
+    data: formdata,
+    }).done(function(data, textStatus, jqXHR) {
+        var api_reply = data.result.score;
+        console.log(api_reply);
+
+        // 小数点第二位未満を四捨五入
+        var score = Math.round(api_reply * 100) / 100;
+        console.log(score)
+        scoreRendering(score);
+
+        // 予想と結果の差の絶対値,四捨五入
+        var difference = Math.round(Math.abs(score - predict));
+        evalRendering(difference);
+
+    }).fail(function(data, textStatus, error) {
+        alert(jqXHR.status);
+        console.log('失敗');
+    });
+
+    // Responseとして返却されたスコアを引数に渡す。
+    
+};
+
+window.send = send;
